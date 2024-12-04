1d7de5a7398ac9c26c9c032b0589bc7b92af57d5
HitotsumeGod <chamcraft@outlook.com>
2024-12-04 07:18:21 -0500

    Update Driving.java

--- Driving.java
+++ Driving.java
@@ -1,14 +1,23 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class Driving {
+public class MikeMath {
 
 	public static void main(String[] args) {
 	
 		Arithmetic a = new Arithmetic();
 		a.createScreen();
+		a.helpScreen();
 	
 	}
 
@@ -16,8 +25,9 @@ public class Driving {
 class Arithmetic {
 
 	public static JFrame f;
-	public static JPanel displays;
-	public static JPanel maths;
+	public static JPanel title;
+	public static JPanel maths; 
+	public static JPanel answer;
 	public static JTextField t;
 	public static JLabel ans;
 	private Font SERIF = new Font("Serif", Font.PLAIN, 20);
@@ -26,31 +36,61 @@ class Arithmetic {
 		
 		Arithmetic a = new Arithmetic();
 		f = new JFrame();
-		displays = new JPanel();
+		title = new JPanel();
 		maths = new JPanel();
-		JLabel l = new JLabel("Aristocles' Arithmetic");
+		answer = new JPanel();
+		JLabel l = new JLabel("Michael's Mathematics");
+		JLabel mike = new JLabel(new ImageIcon("C:\\Users\\chamc\\Downloads\\miker.jpg"));
 		ans = new JLabel("Answer = ");
 		t = new JTextField("                                                                                                                          ");
 		t.addActionListener(new Solver());
 		ans.setFont(a.SERIF);
 		l.setFont(a.SERIF);
-		displays.setLayout(new BoxLayout(displays, BoxLayout.Y_AXIS));
-		maths.setLayout(new BoxLayout(maths, BoxLayout.X_AXIS));
-		displays.add(l);
-		displays.add(t);
-		maths.add(ans);
-		f.add(displays);
+		title.setLayout(new BoxLayout(title, BoxLayout.Y_AXIS));
+		maths.setLayout(new BoxLayout(maths, BoxLayout.Y_AXIS));
+		answer.setLayout(new BoxLayout(answer, BoxLayout.Y_AXIS));
+		title.setPreferredSize(new Dimension(420, 280));
+		maths.setPreferredSize(new Dimension(30, 30));
+		answer.setPreferredSize(new Dimension(210, 100));
+		title.add(l);
+		title.add(mike);
+		maths.add(t);
+		answer.add(ans);
+		f.add(title);
 		f.add(maths);
+		f.add(answer);
 		f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.pack();
 		f.setVisible(true);
+	}
 	
+	public void helpScreen() {
+		
+		JFrame f = new JFrame();
+		JPanel p = new JPanel();
+		JPanel pi = new JPanel();
+		JLabel l = new JLabel("This calculator supports only two variables at the moment. Available operations include: addition - subtraction - multiplication - division.");
+		JLabel end = new JLabel("Developed by Peterbot, with the assistance of dearly departed Ronald Reagan. Copyright 2024.");
+		l.setFont(SERIF);
+		end.setFont(SERIF);
+		p.setLayout(new FlowLayout());
+		pi.setLayout(new FlowLayout());
+		f.setLayout(new BorderLayout());
+		p.add(l);
+		pi.add(end);
+		f.add(p, BorderLayout.CENTER);
+		f.add(pi, BorderLayout.PAGE_END);
+		f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		f.pack();
+		f.setVisible(true);
+		
 	}
 
 }
 class Solver implements ActionListener {
 
+	@SuppressWarnings("static-access")
 	public void actionPerformed(ActionEvent e) {
 	
 		Arithmetic a = new Arithmetic();
@@ -82,7 +122,12 @@ class Solver implements ActionListener {
 			a.ans.setText("Answer = " + fAns);
 			a.f.setVisible(true);
 		
-		} 
+		} else {
+			
+			a.ans.setText("Invalid operator/syntax. Please try again.");
+			a.f.setVisible(true);
+			
+		}
 		
 		
 	
